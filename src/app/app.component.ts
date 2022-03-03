bf4f7db984c6cd312e34f5fa11c4974fdc8a2101
kjford <K@@@shif94>
2022-03-03 08:03:31 -0500

    (config) implemented app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TokenStorageService } from './_services/token-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private roles:string[] = [];
+  isLoggedIn = false;
+  showDoctorBoard = false;
+  showPatientBoard = false;
+  username?: string;
   title = 'patient-management-test';
+
+  constructor(private tokenStorageService: TokenStorageService) { }
+  
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+      this.showDoctorBoard = this.roles.includes('ROLE_DOCTOR');
+      this.showPatientBoard = this.roles.includes('ROLE_PATIENT');
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
 }
